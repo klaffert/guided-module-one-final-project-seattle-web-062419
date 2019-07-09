83abec3b44907793a9f48d049dd7a423c68bb68e
github username <github email address>
2019-07-09 16:54:47 -0700

    Creating methods for CLI

---
+++ lib/CommandLineInterface.rb
@@ -0,0 +1,15 @@
+def greet
+        puts "Welcome to HappyHike, the command line solution for your hike-finding needs!"
+end
+
+def gets_user_input
+        puts "We can help you find which hikes are available wihtin your area."
+        puts "Enter your name here:"
+end
+
+def run
+        greet
+        gets_user_input
+end
+
+
---
+++ lib/api_communicator.rb
@@ -0,0 +1,5 @@
+require 'pry'
+require 'rest-client'
+
+
+all_trail_data = RestClient.get('https://api.transitandtrails.org/api/v1/trips?key=822fe748d6b5a18c7fcffddeedb56d16c29bf0885514c6e09e308ee3530d7703')
