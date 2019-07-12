40866105d7a900d2feb738b49104c5840c8d51a5
github username <github email address>
2019-07-12 15:30:12 -0700

    Final commit

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,10 +3,11 @@ require 'rest-client'
 require 'JSON'
 require_relative 'command_line_interface.rb'
 require_relative '../config/environment.rb'
+require_relative '../config/application.yml'
 
 def get_state_parks(state)
     state = state.upcase
-    all_parks = RestClient.get("https://developer.nps.gov/api/v1/parks?stateCode=#{state}&api_key=rEECihdQAetmTYaSs7lQb0OkMS1e6WDFp7p6lPj5")
+    all_parks = RestClient.get("https://developer.nps.gov/api/v1/parks?stateCode=#{state}ENV[stripe_api_key]")
     parks_hash = JSON.parse(all_parks)
     parks = parks_hash["data"]
     parks_collection = parks.select {|park| park["states"] == state}
@@ -14,11 +15,13 @@ def get_state_parks(state)
     is_looking_at_parks = true
     while is_looking_at_parks
         print_state_parks(parks_collection)
-        puts "Would you like to see more parks? (Y/N)"
+        puts "Would you like to see more parks in #{state}? (Y/N)".bold
         response = gets.chomp
-        if response == "N"
+        if response == "N".upcase
             is_looking_at_parks = false
-            puts "No worries! There are many others to choose from."
+            puts
+            puts "No worries! There are many others to choose from.".bold
+            get_state_parks(gets_state_from_user)
         end
     end
 end
@@ -28,7 +31,8 @@ def print_state_parks(parks_collection)
         puts parks["fullName"] 
         puts
     end
-    puts "Please enter the national park you'd like to visit:"
+    puts "Please enter the national park you'd like to visit:".bold
+    puts
     park = gets.chomp
     park_details = parks_collection.find do |p|
         p["fullName"] == park
@@ -37,27 +41,29 @@ def print_state_parks(parks_collection)
 end
 
 def selected_park(park, park_details, parks_collection)
-    puts park_details["description"]
     puts
-    puts "Would you like to visit? (Y/N?)"
+    puts "You selected #{park}. Here is some additional information about this park:".bold
+    puts
+    puts "Park Description: ".bold + "#{park_details["description"]}"
+    puts
+    puts "Weather Information: ".bold + "#{park_details["weatherInfo"]}"
+    puts
+    puts "Find out more here: ".bold + "#{park_details["url"]}"
+    puts
+    puts "Would you like to visit? (Y/N?)".bold
     puts
     answer = gets.chomp
     puts
-    if answer == "Y"  #if 
+    if answer == "Y".upcase
         puts
-        puts "Please enter your name:"
+        puts "Please enter your name:".bold
         name = gets.chomp
+        puts
         user = User.find_or_create_by(user_name: name)
         new_park = Park.find_or_create_by(park_name: park, state_code: park_details["states"], weather_info: park_details["weatherInfo"], park_url: park_details["url"]) 
-        puts "Nice! You have saved this park for a future visit!"
-     end
+        new_user_park = User_Park.find_or_create_by(user_id: user.id, park_id: new_park.id)
+        puts "Nice #{name}! You have saved this park for a future visit!".bold
+        puts
+    end
 end
 
-
-
-# Find a current_user
-# Create user_park
-# Would you like to see your parks?
-# Puts out park list
-# Would you like to edit park list?
-# Is so, edit or delete park list.
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,6 @@
 def greet
         puts
-        puts "Welcome to National Park Finder, the command line solution for your national park-finding needs!"
+        puts "Welcome to National Park Finder, the command line solution for your national park-finding needs!".bold
         # puts "Please enter your name:"
         # name = gets.chomp
         # user = User.find_or_create_by(user_name: name)
@@ -9,18 +9,21 @@ end
 
 def gets_state_from_user
         puts
-        puts "We can help you find information about national parks."
+        puts "We can help you find information about national parks.".bold
         puts
-        puts "Please enter the two-character state code of your desired state here:"
+        puts "Please enter the two-character state code of your desired state here:".bold
         puts
         state = gets.chomp
+        puts
+        puts "Here is a list of national parks within the state of #{state}:".bold
+        puts
         return state
         puts
 end
 
 def park_choice
         puts
-        puts "Please enter the national park you'd like to visit:"
+        puts "Please enter the national park you'd like to visit:".bold
         park = gets.chomp
         puts
 end
